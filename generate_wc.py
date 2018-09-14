b114e28f25e7f62486a936c9995dddede0793611
Garrett Blinkhorn <gsblinkhorn@gmail.com>
2018-09-14 11:57:59 -0400

    broke functionality to two scripts, deleted stopwords.txt

--- generate_wc.py
+++ generate_wc.py
@@ -2,12 +2,18 @@ from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
 # transform string into wordcloud
 stopwords = set(STOPWORDS)
-with open('stopwords.txt') as f:
-        for word in f.read().split():
-                stopwords.add(word)
+
+#with open('stopwords.txt') as f:
+#       for word in f.read().split():
+#              stopwords.add(word)
+
 text = ""
 with open('output.txt', 'r') as f:
         text = f.read()
 
-wordcloud = WordCloud(stopwords=stopwords, background_color="white", max_words=50).generate(text)
+wordcloud = WordCloud(stopwords=stopwords, 
+			background_color="white", 
+			max_words=100,
+			width=800,
+			height=600).generate(text)
 wordcloud.to_file("wordcloud.png")
