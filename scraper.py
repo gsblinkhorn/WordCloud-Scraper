99711d8aebb1999eae5f1be4a82758a7af5b5746
Garrett Blinkhorn <gsblinkhorn@gmail.com>
2018-09-14 10:29:02 -0400

    Rudimentary wordcloud scrape fucntional

--- scraper.py
+++ scraper.py
@@ -1,35 +1,38 @@
 from bs4 import BeautifulSoup
 from bs4 import NavigableString
 import requests
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 main_link = 'https://www.careerbuilder.com/jobs'
 keywords = 'software developer'
 location = 'Atlanta, GA'
 
 payload = {'keywords': keywords, 'location': location}
- 
-content = requests.get(main_link, params=payload).content
-soup = BeautifulSoup(content.decode('utf-8'), features="html.parser")	
 
-headers = soup.find_all("h2", "job-title")
+content = requests.get(main_link, params=payload).content
+soup = BeautifulSoup(content.decode('utf-8'), features="html.parser")
+
+headers = soup.find_all("h2", {'class': ['job-title', 'show-for-medium-up']})
 links = []
 
 for h2 in headers:
 	a = h2.find("a")
 	links.append(a.attrs['href'])
-	
-main_link = "https://www.careerbuilder.com"	
+
+links = set(links)
+main_link = "https://www.careerbuilder.com"
+words_list = []
+
 for link in links:
-	print(link)
 	url = main_link + link
 	content = requests.get(url).content
 	soup = BeautifulSoup(content.decode('utf-8'), features="html.parser")
 	divs = soup.find_all("div", {"class": "description"})
 
-	words_list = []  
 	for i,div in enumerate(divs):
-		if i % 2 == 0:
-			continue
 		elems = div.children
 		words = " "
 		for elem in elems:
@@ -38,8 +41,13 @@ for link in links:
 			else:
 				words = words + elem.text.strip() + " "
 		words = words.split()
-		words_list.append(words)		
+		words_list.append(words)
 		print(len(words))
-		
-	
 
+text = ""
+for word_list in words_list:
+	for word in word_list:
+		text = text + word + " "
+
+wordcloud = WordCloud().generate(text)
+wordcloud.to_file("wordcloud.png")
