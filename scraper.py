1b466cb0dfbea70af1b199c4f52ec79eaadd743a
Garrett Blinkhorn <gsblinkhorn@gmail.com>
2018-09-14 10:45:16 -0400

    Added stopwords document

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
+# get-request with keywords and location arguments
 main_link = 'https://www.careerbuilder.com/jobs'
 keywords = 'software developer'
 location = 'Atlanta, GA'
@@ -15,18 +16,23 @@ payload = {'keywords': keywords, 'location': location}
 content = requests.get(main_link, params=payload).content
 soup = BeautifulSoup(content.decode('utf-8'), features="html.parser")
 
+# get each job posting
 headers = soup.find_all("h2", {'class': ['job-title', 'show-for-medium-up']})
 links = []
 
+# find the url in each posting
 for h2 in headers:
 	a = h2.find("a")
 	links.append(a.attrs['href'])
 
+# eleiminate duplicate links
 links = set(links)
 main_link = "https://www.careerbuilder.com"
-words_list = []
+text = ""
 
-for link in links:
+# scrap each link for content
+for i, link in enumerate(links):
+	print("Scraping Link " + str(i))
 	url = main_link + link
 	content = requests.get(url).content
 	soup = BeautifulSoup(content.decode('utf-8'), features="html.parser")
@@ -41,13 +47,14 @@ for link in links:
 			else:
 				words = words + elem.text.strip() + " "
 		words = words.split()
-		words_list.append(words)
-		print(len(words))
+		for word in words:
+			text = text + word + " "
 
-text = ""
-for word_list in words_list:
-	for word in word_list:
-		text = text + word + " "
+# transform string into wordcloud
+stopwords = set(STOPWORDS)
+with open('stopwords.txt') as f:
+	for word in f.read().split():
+		stopwords.add(word)
 
-wordcloud = WordCloud().generate(text)
+wordcloud = WordCloud(stopwords=stopwords, background_color="white", max_words=50).generate(text)
 wordcloud.to_file("wordcloud.png")
